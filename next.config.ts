29825b0f1e42708f923e88042a82bff825f7e0b6
Álvaro Limón <alvarolimon.ram@gmail.com>
2025-06-26 14:38:13 -0600

    Changed basepath.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
 	},
 	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
 	trailingSlash: true,
+	basePath: '/family/insurance/app'
 }
  
 module.exports = nextConfig
