aae5b5380cc0bee317e2b19ceff91b926b31492b
Álvaro Limón <alvarolimon.ram@gmail.com>
2025-06-26 14:22:27 -0600

    First origin commit.

---
+++ family_insurance_app/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+	output: 'export',
+	
+	images: {
+		unoptimized: true,
+	},
+	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+	trailingSlash: true,
+}
+ 
+module.exports = nextConfig
